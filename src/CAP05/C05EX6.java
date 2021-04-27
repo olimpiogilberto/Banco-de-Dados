6d8d3d064306145cd9ca7e85d8e62ff867d3dbf3
c129427 <c129427@SP7266ET204.corp.caixa.gov.br>
2021-04-27 18:52:19 -0300

    exer 06 e 07 do CAP05

--- src/CAP05/C05EX6.java
+++ src/CAP05/C05EX6.java
@@ -7,31 +7,53 @@ public class C05EX6 {
 
 	public static void main(String[] args) {
 		
-		int CODIGO;
-		
+		String NOME1, NOME2;
+		int SEXO1, SEXO2, I;
 		
 	    System.out.println();
 	    
-		System.out.print("Entre o código de acesso .....: ");
+		System.out.print("Entre o primeiro nome .....: ");
 		try {
 		BufferedReader br = new BufferedReader(
 		new InputStreamReader(System.in));
-		CODIGO = Integer.parseInt(br.readLine()); }  
+		NOME1 = br.readLine(); }  
 		catch (Exception e) {
-		CODIGO = 0;
+			NOME1 = "";
 		}
-		   
-		if( CODIGO == 1 || CODIGO == 2 || CODIGO == 3)
+		
+		System.out.print("Entre com o sexo: M --> [1] // F -- [2]  .....: ");
+		try {
+		BufferedReader br = new BufferedReader(
+		new InputStreamReader(System.in));
+		SEXO1 = Integer.parseInt(br.readLine()); }  
+		catch (Exception e) {
+			SEXO1 = 0;
+		}
+		
+		System.out.print("Entre o segundo nome .....: ");
+		try {
+		BufferedReader br = new BufferedReader(
+		new InputStreamReader(System.in));
+		NOME2 = br.readLine(); }  
+		catch (Exception e) {
+			NOME2 = "";
+		}
+		
+		System.out.print("Entre com o sexo: M --> [1] // F -- [2]  .....: ");
+		try {
+		BufferedReader br = new BufferedReader(
+		new InputStreamReader(System.in));
+		SEXO2 = Integer.parseInt(br.readLine()); }  
+		catch (Exception e) {
+			SEXO2 = 0;
+		}
+		   //esse símbolo ^ inverte o resultado lógico
+		if( SEXO1 == 1 ^ SEXO2 == 2)
 		{
-			if(CODIGO == 1)
-				System.out.print("um");
-			if(CODIGO == 2)
-				System.out.print("dois");
-			if(CODIGO == 3)
-				System.out.print("tres");
+			System.out.print(NOME1 + " pode se casar com " + NOME2);
 			
 		}
 		else
-			System.out.print("Código inválido .....: ");
+			System.out.print(NOME1 + " não pode se casar com " + NOME2);
 	}
 }
