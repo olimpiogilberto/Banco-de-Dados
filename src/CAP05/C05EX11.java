94d52e928aa023531adae7973ceede3f1be328ea
c129427 <c129427@SP7266ET204.corp.caixa.gov.br>
2021-04-30 11:18:07 -0300

    gerando arquivos

---
+++ src/CAP05/C05EX11.java
@@ -0,0 +1,61 @@
+package CAP05;
+
+import java.io.*;
+public class C05EX11 {
+
+	public static void main(String[] args) {
+		
+		float A, B, R = 0;
+		int OPCAO;
+		
+	    System.out.println();
+	    
+		System.out.print("Entre o valor <A>: ");
+		try {
+			BufferedReader br = new BufferedReader(
+			new InputStreamReader(System.in));
+			A = Integer.parseInt(br.readLine()); }  
+			catch (Exception e) {
+				A = 0;
+			}
+		
+		System.out.print("Entre o valor <B>: ");
+		try {
+			BufferedReader br = new BufferedReader(
+			new InputStreamReader(System.in));
+			B = Integer.parseInt(br.readLine()); }  
+			catch (Exception e) {
+				B = 0;
+			}
+		
+		System.out.println();
+		System.out.print("[1] - Adição");
+		System.out.print("[2] - Subtração ");
+		System.out.print("[3] - Multiplicação ");
+		System.out.print("[4] - Divisão ");
+		System.out.println();
+		System.out.print("Escolha uma opção: ");
+		try {
+			BufferedReader br = new BufferedReader(
+			new InputStreamReader(System.in));
+			OPCAO = Integer.parseInt(br.readLine()); }  
+			catch (Exception e) {
+			OPCAO = 0;
+			}
+		
+		if(OPCAO == 1)
+			R = A + B;
+		if(OPCAO == 2)
+		    R = A - B;
+		if(OPCAO == 3)
+			R = A * B;
+		if(OPCAO == 4)
+			if(B==0)
+			  R = 0;
+			else
+			  R = A / B;			
+			
+		System.out.print("O resultado equivale à " + R);
+	System.out.println();
+	}
+}
