377cfafb0cb23acd0e68a19003c6cacb7168d623
c129427 <c129427@SP7266ET204.corp.caixa.gov.br>
2021-05-03 11:48:46 -0300

    Exer 01, 02 do CAP06

---
+++ src/CAP06/C06EX01.java
@@ -0,0 +1,20 @@
+package CAP06;
+
+public class C06EX01 {
+
+	public static void main(String[] args) {
+		
+		int I;
+		
+		
+	    System.out.println();
+	    
+		I = 1;
+		while (I <= 5) {
+			System.out.print("Valor = " + I);
+			System.out.println();
+			I = I + 1;
+		}
+		
+	}
+}
