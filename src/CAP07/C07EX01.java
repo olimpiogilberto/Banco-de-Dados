6efcdae5e1b56161712989c7cb8d683ca5378511
c129427 <c129427@SP7266ET204.corp.caixa.gov.br>
2021-05-06 11:31:54 -0300

    exer 01 ao 04 CAP07

---
+++ src/CAP07/C07EX01.java
@@ -0,0 +1,18 @@
+package CAP07;
+
+public class C07EX01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println();
+		System.out.println(Math.acos(-1));
+		System.out.println(Math.acos(1));
+		System.out.println(Math.acos(0.5));
+		System.out.println(Math.acos(0.5) * 180 / Math.PI);
+		System.out.println(Math.acos(Math.toDegrees(Math.acos(0.5))));
+	    System.out.println();
+	    
+
+		
+	}
+}
---
+++ src/CAP07/C07EX02.java
@@ -0,0 +1,18 @@
+package CAP07;
+
+public class C07EX02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println();
+		System.out.println(Math.asin(-1));
+		System.out.println(Math.asin(1));
+		System.out.println(Math.asin(0.5));
+		System.out.println(Math.asin(0.5) * 180 / Math.PI);
+		System.out.println(Math.asin(Math.toDegrees(Math.asin(0.5))));
+	    System.out.println();
+	    
+
+		
+	}
+}
