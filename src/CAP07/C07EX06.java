a69759001ba6b2c9a934cd2f1e8765d919d8d1cf
[Gilberto Olimpio] <[olimpiogilberto@hotmail.com]>
2021-08-03 14:28:49 -0300

    teste

---
+++ src/CAP07/C07EX06.java
@@ -0,0 +1,16 @@
+package CAP07;
+
+public class C07EX06 {
+
+	public static void main(String[] args) {
+		
+
+			System.out.println();
+	        System.out.println( Math.sin(30 * Math.toRadians(1)));
+	        System.out.println( Math.sin(Math.PI / 1));
+	        System.out.println( Math.sin(1));
+	        System.out.println( Math.sin(Math.PI / 3));
+					
+		}
+	}
+
